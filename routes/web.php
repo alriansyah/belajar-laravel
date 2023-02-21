0af4d1052c662208e8e51c60d399f72a629db81b
alriansyah <alriansyah1218@gmail.com>
2023-02-22 01:34:57 +0700

    Belajar Blade part-1

--- routes/web.php
+++ routes/web.php
@@ -14,43 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
-
-Route::get('/bebas', function () {
-    return "Cetak string aja";
-});
-
-// view route standar
-Route::view('/contact', 'contact');
-
-// mengirim argumen ke view
-Route::view('/perkenalan', 'perkenalan', ['name' => 'Al Riansyah', 'alamat' => 'Pelita Jaya', 'phone' => '085777889922']);
-
-// Redirect
-// Route::redirect('/contact', '/contact-us');
-
-// Route Parameters
-Route::get('/product/{id}', function (string $id) {
-    return view('product.detail', ['id' => $id]);
-});
-
-
-// Route Prefixes
-Route::prefix('admin')->group(function () {
-    Route::get('/contact', function () {
-        return 'Halaman Kontak Admin';
-    });
-    
-    Route::get('/about', function () {
-        return 'Halaman Tentang Admin';
-    });
-    
-    Route::get('/profile', function () {
-        return 'Halaman Profile Admin';
-    });
+    return view('home', [
+        'name' => 'Al Riansyah', 
+        'alamat' => 'Desa Pelita Jaya', 
+        'pekerjaan' => 'Fullstack Developer',
+        'role' => 'user',
+        'buah' => ['Apel', 'Mangga', 'Jeruk', 'Semangka', 'Melon', 'Strawberry']
+    ]);
 });
