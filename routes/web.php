c39bea52d606d783e3ad79f30b1445665d2f6fd7
alriansyah <alriansyah1218@gmail.com>
2023-02-21 15:16:32 +0700

    Belajar Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/bebas', function () {
+    return "Cetak string aja";
+});
+
+// view route standar
+Route::view('/contact', 'contact');
+
+// mengirim argumen ke view
+Route::view('/perkenalan', 'perkenalan', ['name' => 'Al Riansyah', 'alamat' => 'Pelita Jaya', 'phone' => '085777889922']);
+
+// Redirect
+// Route::redirect('/contact', '/contact-us');
+
+// Route Parameters
+Route::get('/product/{id}', function (string $id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+
+// Route Prefixes
+Route::prefix('admin')->group(function () {
+    Route::get('/contact', function () {
+        return 'Halaman Kontak Admin';
+    });
+    
+    Route::get('/about', function () {
+        return 'Halaman Tentang Admin';
+    });
+    
+    Route::get('/profile', function () {
+        return 'Halaman Profile Admin';
+    });
+});
