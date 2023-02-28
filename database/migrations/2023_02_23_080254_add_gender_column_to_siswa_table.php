dee8d7fba3142e63aa5520db25f940e5d425b526
alriansyah <alriansyah1218@gmail.com>
2023-02-28 20:30:51 +0700

    Belajar Migration

---
+++ database/migrations/2023_02_23_080254_add_gender_column_to_siswa_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('siswa', function (Blueprint $table) {
+            $table->string('gender', 10)->required()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('siswa', function (Blueprint $table) {
+            //drop columns
+            $table->dropColumn('gender');
+        });
+    }
+};
--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -8,6 +8,12 @@
     <title>Laravel 10 | @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 
 <body>
