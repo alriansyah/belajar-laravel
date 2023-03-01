18554c2da219c464e1e91d39d8138c880194f4fd
alriansyah <alriansyah1218@gmail.com>
2023-03-02 01:34:35 +0700

    Factory Faker Laravel 10

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -15,28 +15,32 @@ class SiswaSeeder extends Seeder
      */
     public function run(): void
     {
-        Schema::disableForeignKeyConstraints();
-        Siswa::truncate(); // menghapus/menosongkan seluruh data pada table
-        Schema::enableForeignKeyConstraints();
+        // Schema::disableForeignKeyConstraints();
+        // Siswa::truncate(); // menghapus/menosongkan seluruh data pada table
+        // Schema::enableForeignKeyConstraints();
 
-        $dataSiswa = [
-            ['name' => 'Madara', 'gender' => 'L', 'nim' => '0101001', 'class_id' =>1],
-            ['name' => 'Obito', 'gender' => 'L', 'nim' => '0101002', 'class_id' =>1],
-            ['name' => 'Izuna', 'gender' => 'L', 'nim' => '0101003', 'class_id' =>2],
-            ['name' => 'Sasuke', 'gender' => 'L', 'nim' => '0101004', 'class_id' =>2],
-            ['name' => 'Itachi', 'gender' => 'L', 'nim' => '0101005', 'class_id' =>3],
-            ['name' => 'Shisui', 'gender' => 'L', 'nim' => '0101006', 'class_id' =>3],
-        ];
+        // $dataSiswa = [
+        //     ['name' => 'Madara', 'gender' => 'L', 'nim' => '0101001', 'class_id' =>1],
+        //     ['name' => 'Obito', 'gender' => 'L', 'nim' => '0101002', 'class_id' =>1],
+        //     ['name' => 'Izuna', 'gender' => 'L', 'nim' => '0101003', 'class_id' =>2],
+        //     ['name' => 'Sasuke', 'gender' => 'L', 'nim' => '0101004', 'class_id' =>2],
+        //     ['name' => 'Itachi', 'gender' => 'L', 'nim' => '0101005', 'class_id' =>3],
+        //     ['name' => 'Shisui', 'gender' => 'L', 'nim' => '0101006', 'class_id' =>3],
+        // ];
 
-        foreach ($dataSiswa as $value) {
-            Siswa::insert([
-                'name' => $value['name'],
-                'gender' => $value['gender'],
-                'nim' => $value['nim'],
-                'class_id' => $value['class_id'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
+        // foreach ($dataSiswa as $value) {
+        //     Siswa::insert([
+        //         'name' => $value['name'],
+        //         'gender' => $value['gender'],
+        //         'nim' => $value['nim'],
+        //         'class_id' => $value['class_id'],
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ]);
+        // }
+
+        Siswa::factory()
+            ->count(20)
+            ->create();
     }
 }
